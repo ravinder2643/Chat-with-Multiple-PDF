12054df728d16606e5fdc08f08f1677e1006c6b7
ravinder2643 <138236210+ravinder2643@users.noreply.github.com>
2024-05-28 21:12:08 +0530

    added get_coversation_chain function

--- chatWithPDF.py
+++ chatWithPDF.py
@@ -16,9 +16,6 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
 
-
-
-
 def get_pdf_text(pdf_docs):
     text=""
     for pdf in pdf_docs:
@@ -38,5 +35,25 @@ def get_vector_store(text_chunks):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
+
+
+def get_conversational_chain():
+
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                             temperature=0.3)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
 
 
